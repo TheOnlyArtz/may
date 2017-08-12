853c7eb1f509169e502363031bf1328ed2198b47
TheOnlyArtz <gamesil456@gmail.com>
2017-08-12 22:08:27 +0300

    Fixed function

--- functions/checkPerms.js
+++ functions/checkPerms.js
@@ -7,18 +7,17 @@ function checkForPermissions(botPermissions, authorPermissions, cmd, msg, client
     throw Error(`You must specify command name`);
   }
 
-  for (var i = 0; i < cmd.help.botPerm.length; i++) {
+  for (let i = 0; i < cmd.help.botPerm.length; i++) {
     if (!msg.guild.member(client.user).hasPermission(botPermissions[i])) {
-      console.log(cmd.help.botPerm[i]);
-      if (i === 2) return;
-        msg.channel.send(`I cannot make it through the command Reason: Missing permissions (` + cmd.help.botPerm[i] + ")")
+      console.log();
+        msg.channel.send(`I cannot make it through the command Reason: Missing permissions (` + cmd.help.botPerm + ' ' + ")")
         .catch(e => {
-          logger.error(e)
           msg.guild.owner.send(`I cannot make it through the command Reason: Missing permissions (` + cmd.help.botPerm[i] + ")")
         });
     }
+    if (i === 2) return;
   }
-  for (var i = 0; i < cmd.help.authorPerm.length; i++) {
+  for (let i = 0; i < cmd.help.authorPerm.length; i++) {
     if (!msg.guild.member(msg.author).hasPermission(authorPermissions[i])) {
       return msg.channel.send(`I cannot make it through the command Reason: Missing permissions (${cmd.help.authorPerm[i]})`)
         .catch(e => {
