6726de0eea81d4b005808f56009a23fa8fc46074
Charlie <charlie.tiehm@gmail.com>
2017-08-12 18:06:58 +0200

    Added '-' to Alias

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,13 @@
 exports.run = async(client, msg, args) => {
-  let pingMsg = await msg.channel.send('Pinging...')
+  let pingMsg = await msg.channel.send('Pinging...');
   pingMsg.edit(`so fast! took ${pingMsg.createdTimestamp - msg.createdTimestamp}ms`)
-}
+};
 
 exports.help = {
     usage: 'The command takes no arguments',
     description: 'I will reply with pong fast as possible',
     detail: 'When using ping the bot will display you the response time in ms',
     alias: [
-
+        '-'
     ]
 };
