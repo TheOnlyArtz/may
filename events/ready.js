7d74d4b6983818632d46841fe61333e1842a1817
Charlie <charlie.tiehm@gmail.com>
2017-08-12 18:42:50 +0200

    changed === false to !

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 exports.run = (client) => {
     logger.info('May is ready to use');
     logger.debug(`Logged in as ${client.user.tag}`);
-    logger.debug(`Serving ${client.guilds.size} servers with ${client.users.filter(i => i.bot === false).size} users`);
+    logger.debug(`Serving ${client.guilds.size} servers with ${client.users.filter(i => !i.bot).size} users`);
     client.user.setGame('tests').catch( err => logger.error(err));
 };
