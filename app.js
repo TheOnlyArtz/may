fefcf970de91ff29cb284630ab90eb1d7f85d93b
TheOnlyArtz <gamesil456@gmail.com>
2017-08-12 18:14:50 +0300

    Finished basic setup

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./config/config.json')
+const Discord     = require('discord.js');
+const client      = new Discord.Client();
+const config      = require('./config/config.json');
+const loggerClass = require('artzlogger');
+logger            = new loggerClass({timeStamp: moment(new Date).format('hh:mm:ss:')})
 client.login(config.TOKEN)
+
+client.on('ready', async () => {
+  logger.info('May has now been connected!')
+  logger.debug(`May's ID: ${client.user.id}`)
+  logger.debug(`May's Tag: ${client.user.tag}`)
+});
