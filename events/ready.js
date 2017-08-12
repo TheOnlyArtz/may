bed217201631269c2cbe3a0842b0743a34d3eebc
TheOnlyArtz <gamesil456@gmail.com>
2017-08-12 19:42:21 +0300

    Filtering bots

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 exports.run = (client) => {
     logger.info('May is ready to use');
     logger.debug(`Logged in as ${client.user.tag}`);
-    logger.debug(`Serving ${client.guilds.size} servers with ${client.users.size} users`);
+    logger.debug(`Serving ${client.guilds.size} servers with ${client.users.filter(i => i.bot === false).size} users`);
     client.user.setGame('tests').catch( err => logger.error(err));
 };
