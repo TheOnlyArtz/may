367099dcd1aa55ac9930678bdb3476d0160f5ecd
Charlie <charlie.tiehm@gmail.com>
2017-08-12 21:35:35 +0200

    Spongebob Meme text

---
+++ commands/sponge.js
@@ -0,0 +1,18 @@
+const sponge = require('spongeuscator');
+
+exports.run = async (client,msg,args) => {
+    if (args.join(' ').length < 4) return msg.channel.send('Please give a message with more than 4 chars');
+    msg.channel.send(sponge(args.join(' ')));
+};
+
+exports.help = {
+    usage: '[text]',
+    description: 'Spooongebob',
+    detail: 'Spooongebob',
+    botPerm    : ['SEND_MESSAGES'],
+    authorPerm : ['SEND_MESSAGES'],
+    category : "fun",
+    alias      : [
+        null
+    ]
+};
