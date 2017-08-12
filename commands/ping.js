a51abf28cc529896090bb1c1dc132fc3d2c1c6eb
TheOnlyArtz <gamesil456@gmail.com>
2017-08-12 20:00:59 +0300

    Permission checking

--- commands/ping.js
+++ commands/ping.js
@@ -4,10 +4,12 @@ exports.run = async(client, msg, args) => {
 };
 
 exports.help = {
-    usage: 'The command takes no arguments',
+    usage      : 'The command takes no arguments',
     description: 'I will reply with pong fast as possible',
-    detail: 'When using ping the bot will display you the response time in ms',
-    alias: [
+    detail     : 'When using ping the bot will display you the response time in ms',
+    botPerm    : ['SEND_MESSAGES'],
+    authorPerm : ['SEND_MESSAGES'],
+    alias      : [
         null
     ]
 };
