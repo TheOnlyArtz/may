37f3d6ffd0be877956128d61def791e30bf7b20f
TheOnlyArtz <gamesil456@gmail.com>
2017-08-12 21:47:11 +0300

    You dont need to set avatar everytime you ready

--- commands/purge.js
+++ commands/purge.js
@@ -6,9 +6,6 @@ exports.run = async(client, msg, args) => {
     return msg.channel.send('Please specify amount of messages to delete.');
   }
 
-  // if (typeof amount !== 'number') {
-  //     return msg.channel.send('Amount has to be a number.')
-  // }
 
   if (actualAmountInt > 99) {
     return msg.channel.send('The max I can delete are 100 messages at once')
@@ -20,13 +17,16 @@ exports.run = async(client, msg, args) => {
     r.map(o => o.channel.bulkDelete(actualAmountInt))
     logger.info(`Deleted ${actualAmountInt - 1} messages inside ${r.map(o => o.channel.name)[0]}`)
   })
+    .catch(e => {
+      msg.channel.send('Amount has to be a number.')
+    })
 };
 
 exports.help = {
     category   : 'util',
     usage      : 'The command takes no arguments',
     description: 'I will reply with pong fast as possible',
-    detail     : 'When using ping the bot will display you the response time in ms',
+    detail     : 'When using purge the bot delete the amount of requested messages',
     botPerm    : ['MANAGE_MESSAGES', "SEND_MESSAGES"],
     authorPerm : ['MANAGE_MESSAGES', "SEND_MESSAGES"],
     alias      : [
--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,4 @@ exports.run = (client) => {
     logger.debug(`Logged in as ${client.user.tag}`);
     logger.debug(`Serving ${client.guilds.size} servers with ${client.users.filter(i => !i.bot).size} users`);
     client.user.setGame('tests').catch( err => logger.error(err));
-    client.user.setAvatar('https://cdn.discordapp.com/avatars/345947240612888580/37fef8675013f95c8e85bbb3510c54c0.png?size=2048').catch(err => logger.error(err))
 };
