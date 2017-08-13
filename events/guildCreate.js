ae6862b6e2a8b20ded01bbb793a921427a3ff6f2
TheOnlyArtz <gamesil456@gmail.com>
2017-08-13 10:09:36 +0300

    added guildCreate

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,6 @@
-const config = require('../config/config.json').TOKEN
+const config = require('../config/config.json').PREFIX
 exports.run = async (client, guild) => {
-  guild.defaultChannel.send(`Hey :wave:, Thank you very much for adding me to ${guild.name} to get access to the command list do \`${config}\``);
+
+  let Id = guild.channels.get(guild.channels.filter(i=> i.type === "text").map(i => i)[0].id);
+  Id.send(`Hey :wave:, Thank you very much for adding me to ${guild.name} to get access to the command list do \`${config}help\``);
 }
