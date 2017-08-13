27d2c9b4cfa0288c0341ec4df5e2c6fb867f44b4
Charlie <charlie.tiehm@gmail.com>
2017-08-13 12:19:30 +0200

    Ignore the content of this shit I am working on it

--- commands/help.js
+++ commands/help.js
@@ -1,29 +1,120 @@
 const
     discord = require('discord.js'),
     config = require('../config/config.json'),
-    fs = require('fs');
+    fs = require('fs'),
+    alias = require('../events/message.js').alias;
 
 exports.run = async (bot,msg,args) => {
     if (args[0]) {
         msg.delete();
-        try {
-            let
-                cmd = require('./' + args[0]),
-                help = cmd.help,
-                usage = config.PREFIX + args[0] + ' ' + help.usage,
-                detail = help.detail,
-                alias = help.alias.join(', '),
-                embed = new discord.RichEmbed()
-                    .setTitle('Command Information | ' + args[0])
-                    .setDescription(detail)
-                    .addField('Usage', usage)
-                    .addField('Alias', alias ? alias : 'None')
-                    .setTimestamp()
-                    .setFooter(bot.user.username);
-            msg.channel.send({embed});
-        } catch (err) {
-            logger.error(err);
-            msg.channel.send(`:x: ${config.PREFIX}${args[0]} is no command`)
+        if (args[0].toLowerCase() === 'fun') {
+            let comm = [];
+            fs.readdir('./commands/', (err,files) => {
+                if (err) logger.error(err);
+                files.forEach(file => {
+                    let info = require('./' + file);
+                    if (info.help.category === 'fun') {
+                        comm.push(`**${config.PREFIX}${file.split('.')[0]}** ${info.help.description}`)
+                    }
+                });
+                let toSend = comm.join('\n');
+                embedMessage.descEmbed({
+                    type: 'desc',
+                    content: toSend
+                })
+            });
+
+        }
+        else if (args[0].toLowerCase() === 'util') {
+            let comm = [];
+            fs.readdir('./commands/', (err,files) => {
+                if (err) logger.error(err);
+                files.forEach(file => {
+                    let info = require('./' + file);
+                    if (info.help.category === 'util') {
+                        comm.push(`**${config.PREFIX}${file.split('.')[0]}** ${info.help.description}`)
+                    }
+                });
+                let toSend = comm.join('\n');
+                embedMessage.descEmbed({
+                    type: 'desc',
+                    content: toSend
+                })
+            });
+
+        }
+        else if (args[0].toLowerCase() === 'moderation') {
+            let comm = [];
+            fs.readdir('./commands/', (err,files) => {
+                if (err) logger.error(err);
+                files.forEach(file => {
+                    let info = require('./' + file);
+                    if (info.help.category === 'moderation') {
+                        comm.push(`**${config.PREFIX}${file.split('.')[0]}** ${info.help.description}`)
+                    }
+                });
+                let toSend = comm.join('\n');
+                embedMessage.descEmbed({
+                    type: 'desc',
+                    content: toSend
+                })
+            });
+
+        }
+        else if (args[0].toLowerCase() === 'music') {
+            let comm = [];
+            fs.readdir('./commands/', (err,files) => {
+                if (err) logger.error(err);
+                files.forEach(file => {
+                    let info = require('./' + file);
+                    if (info.help.category === 'music') {
+                        comm.push(`**${config.PREFIX}${file.split('.')[0]}** ${info.help.description}`)
+                    }
+                });
+                let toSend = comm.join('\n');
+                embedMessage.descEmbed({
+                    type: 'desc',
+                    content: toSend
+                })
+            });
+
+        }
+        else {
+            try {
+                let
+                    cmd = require('./' + args[0]),
+                    help = cmd.help,
+                    usage = config.PREFIX + args[0] + ' ' + help.usage,
+                    detail = help.detail,
+                    alias3 = help.alias.join(', '),
+                    embed = new discord.RichEmbed()
+                        .setTitle('Command Information | ' + args[0])
+                        .setDescription(detail)
+                        .addField('Usage', usage)
+                        .addField('Alias', alias3 ? alias3 : 'None')
+                        .setTimestamp()
+                        .setFooter(bot.user.username);
+                msg.channel.send({embed});
+            } catch (err) {
+                if (alias[args[0]]) {
+                    let cmd = require('./' + alias[args[0]] + '.js'),
+                        help = cmd.help,
+                        usage = config.PREFIX + alias[args[0]] + ' ' + help.usage,
+                        detail = help.detail,
+                        alias2 = help.alias.join(', '),
+                        embed = new discord.RichEmbed()
+                            .setTitle('Command Information | ' + alias[args[0]])
+                            .setDescription(detail)
+                            .addField('Usage', usage)
+                            .addField('Alias', alias2 ? alias2 : 'None')
+                            .setTimestamp()
+                            .setFooter(bot.user.username);
+                    msg.channel.send({embed});
+                }
+                else {
+                    msg.channel.send(`:x: ${config.PREFIX}${args[0]} is no command`)
+                }
+            }
         }
     }
     else {
@@ -33,6 +124,7 @@ exports.run = async (bot,msg,args) => {
         fs.readdir('./commands/', (err, files) => {
             if (err) return logger.error(err);
             files.forEach(file => {
+
                 let help = "";
                 count++;
                 let
@@ -40,9 +132,20 @@ exports.run = async (bot,msg,args) => {
                     helpName = file.split('.')[0],
                     info = helpInfo.help,
                     description = info.description;
-                help += `**${config.PREFIX}${helpName}** ${description}\n`;
+                if (info.category === 'fun') {
+                    funCom.push(`**${config.PREFIX}${helpName}** ${description}`)
+                }
+                else if (info.category === 'util') {
+                    utilCom.push(`**${config.PREFIX}${helpName}** ${description}`)
+                }
+                else if (info.category === 'moderation') {
+                    modCom.push(`**${config.PREFIX}${helpName}** ${description}`)
+                }
+                else if (info.category === 'music') {
+                    musicCom.push(`**${config.PREFIX}${helpName}** ${description}`)
+                }
                 let newBatch = batch + help;
-                if (newBatch.length > (1024 - 8)) {
+                if (newBatch.length > (1024 - 8 - help2.length)) {
                     msg.channel.send({
                       embed : {
                         description : newBatch
