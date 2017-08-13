cffaeb196ff2131d6d706a0b930bcd2e95c77a32
TheOnlyArtz <gamesil456@gmail.com>
2017-08-13 10:18:28 +0300

    Done a description for embed

---
+++ classes/embedMessage.js
@@ -0,0 +1,20 @@
+const Discord = require('dicsord.js')
+class embedMessage {
+  constructor(options, msg) {
+    this.options = options
+    this.message = msg
+  }
+
+  descEmbed() {
+    if (this.options.type === "desc" ) {
+      if (!this.options.content) {
+        throw Error 'You did not specify the text inside the constructor options.'
+      } else {
+        const embed = new Discord.RichEmbed()
+        .setDescription(this.options.content)
+      }
+    } else {
+      throw Error 'You chose descEmbed but your option does not have "desc" in it.'
+    }
+  }
+}
