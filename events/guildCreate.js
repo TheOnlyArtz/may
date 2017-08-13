62f2e58ab06afabab877b78b2c13499b7e8a798e
TheOnlyArtz <gamesil456@gmail.com>
2017-08-13 09:46:59 +0300

    Fixed guildCreate

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,4 @@
 const config = require('../config/config.json').TOKEN
-exports.run = async (client, member) => {
-  const guild = member.guild;
+exports.run = async (client, guild) => {
   guild.defaultChannel.send(`Hey :wave:, Thank you very much for adding me to ${guild.name} to get access to the command list do \`${config}\``);
 }
