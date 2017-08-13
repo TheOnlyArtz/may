3b78254a29ea2348de8f8a45bca7830117e21817
TheOnlyArtz <gamesil456@gmail.com>
2017-08-13 11:13:19 +0300

    Added comments

--- classes/embedMessage.js
+++ classes/embedMessage.js
@@ -21,29 +21,60 @@ class embedMessage {
     }
   }
   advanced(options) {
+    /**
+    *@returns {Error}
+    */
     if (!options) {
       throw Error('You must specify at list one option, list {desc, footer, thumbnail, title, author fields[field1, content, field2, content]}')
     }
+
+    /**
+    *@returns {Error}
+    */
     if (!options.fields[0]) {
       throw Error('You must add atleast 1 field')
     }
+
     const embed = new Discord.RichEmbed();
+
+    /**
+    *@returns {Object}
+    *@param {string} title
+    *@param {string} content
+    */
     for (let i = 0; i < options.fields.length; i++) {
       embed.addField(options.fields[i].title, options.fields[i].content)
     }
+
+    /**
+    *@param {String} desc
+    */
     if (options.desc) {
       embed.setDescription(options.desc)
     }
+
+    /**
+    *@param {String} footer
+    */
     if (options.footer) {
       embed.setFooter(options.footer)
     }
+
+    /**
+    *@param {String} title
+    */
     if(options.title) {
       embed.setTitle(options.title)
     }
+
+    /**
+    *@param {String} thumbnail
+    */
     if (options.thumbnail) {
       embed.setThumbnail(options.thumbnail)
     }
-    this.message.channel.send({embed})
+
+    this.message.channel.send({embed}) //Send the embed after all the options been selected
   }
 }
 module.exports = embedMessage;
