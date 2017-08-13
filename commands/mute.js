e1f8d418b963f213ed6acdfe3217016bc64b62c5
TheOnlyArtz <gamesil456@gmail.com>
2017-08-13 15:18:02 +0300

    More variables

--- commands/mute.js
+++ commands/mute.js
@@ -1,19 +1,37 @@
 exports.run = async(client, msg, args) => {
+  const ms = require('ms')
   /**
   * @var {target} @type {Object} user-Object to mute
   * @var {dole} @type {Object} role-Object for mutes
   * @var {reason} @type {String} Punishment-Reason
   * @var {duration} @type {String} Punishment-Duration
   */
-  const target = message.mentions.users.first();
+  const target = msg.mentions.users.first();
   const role = msg.guild.roles.find('name', 'may-muted');
-  const reason = message.content.split(' ').slice(2).join(' ');
-  const duration = message.content.split(' ')[1];
+  const reason = msg.content.split(' ').slice(2).join(' ');
+  const duration = msg.content.split(' ')[1];
+  const mayLog = msg.guild.channels.find('name', 'may-log');
+
+  let Freason;
+  if (!reason) {
+    Freason = 'None'
+  } else {
+    Freason = reason;
+  }
+
+  let Ftime;
+  if (duration === 'permant') {
+    Ftime = 'permant'
+  } else {
+    Ftime = ms(duration)
+  }
+
+  console.log(Ftime, Freason);
 };
 
 exports.help = {
     category   : 'moderation',
-    usage      : '[time:optional] [reason:optional]',
+    usage      : '[time:time or permant] [reason:optional]',
     description: 'I will be sure to shut their mouth',
     detail     : 'When using mute the bot will mute the selected user, for the time you choose(optional)',
     botPerm    : ['SEND_MESSAGES', 'MANAGE_ROLES'],
