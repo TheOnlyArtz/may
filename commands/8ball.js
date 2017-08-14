e51ede267860d0ab4d5b59ea652601c61565ce15
TheOnlyArtz <gamesil456@gmail.com>
2017-08-14 16:25:17 +0300

    added comments to 8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,11 +1,18 @@
 const snekfetch = require('snekfetch');
 
 exports.run = async (client,msg,args) => {
+    // Handle error and ask for arguments
     if (!args.join(' ')) return msg.channel.send('Please include a question');
+    
+    /**
+    * @param {String} url fetch data from
+    * @returns {Promise}
+    * @returns {Error}
+    */
     let r = await snekfetch.get("https://8ball.delegator.com/magic/JSON/0");
     let answerBall = r.body;
     let ball = answerBall.magic.answer;
-    msg.channel.send("**[8 Ball]** :crystal_ball: " + ball);
+    msg.channel.send("**[8 Ball]** :crystal_ball: " + ball)
 };
 
 exports.help = {
@@ -14,7 +21,7 @@ exports.help = {
     description: 'Ask the magic 8 ball something',
     detail: 'Ask the magic 8 ball something',
     botPerm    : ['SEND_MESSAGES'],
-    authorPerm : ['SEND_MESSAGES'],
+    authorPerm : [null],
     alias      : [
         '8b'
     ]
