b26d5421b8e4828d12e1f065f2bc2a7a878a71cd
Charlie <charlie.tiehm@gmail.com>
2017-08-15 17:39:11 +0200

    Added cooldown

--- commands/rule34.js
+++ commands/rule34.js
@@ -1,20 +1,23 @@
+const cooldown = require('../functions/cooldown.js');
 const snekfetch = require('snekfetch');
 const xml2js = require('xml2js');
 // TODO: Beautify a bit
 // TODO: Add database check if channel is nsfw also make a make channel nsfw command
 exports.run = async(client, msg, args) => {
-    if (!args.join(' ')) return msg.channel.send("Please give a search terms!");
-    snekfetch.get('http://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + args.join(' ')).then(r => {
-        if (r.body.length < 75) return msg.channel.send(":x: Nothing found!");
-        xml2js.parseString(r.body, (err, reply) => {
-            if (err) {
-                return msg.channel.send('The API returned an unconventional response. :thinking:')
-            } else {
-                let count = Math.floor((Math.random() * reply.posts.post.length));
-                msg.channel.send({files: [reply.posts.post[count].$.file_url]}).catch(logger.error);
-            }
+    if (cooldown(msg, 'rule34', 60, 'This command has a cooldown of **1 Minute**!')) {
+        if (!args.join(' ')) return msg.channel.send("Please give a search terms!");
+        snekfetch.get('http://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + args.join(' ')).then(r => {
+            if (r.body.length < 75) return msg.channel.send(":x: Nothing found!");
+            xml2js.parseString(r.body, (err, reply) => {
+                if (err) {
+                    return msg.channel.send('The API returned an unconventional response. :thinking:')
+                } else {
+                    let count = Math.floor((Math.random() * reply.posts.post.length));
+                    msg.channel.send({files: [reply.posts.post[count].$.file_url]}).catch(logger.error);
+                }
+            })
         })
-    })
+    }
 };
 // TODO: Add real stuff to the help I am too lazy rn
 exports.help = {
