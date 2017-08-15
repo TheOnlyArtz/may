44491fb029e0d9e447f3235af05d6d0993c68466
Charlie <charlie.tiehm@gmail.com>
2017-08-15 18:28:32 +0200

    Edited so it doesn't break shit, also changed var to let, also added .catch

--- commands/google.js
+++ commands/google.js
@@ -1,26 +1,26 @@
-const Discord = require('discord.js')
-const google = require('google')
+const Discord = require('discord.js');
+const google = require('google');
 exports.run = (client, msg, args) => {
-  const toSearch = args.join(' ')
-  let links = []
+  const toSearch = args.join(' ');
+  let links = [];
   if (!toSearch) {
     return msg.reply('Please insert something to search');
   }
 
   google(toSearch, async(err, res) => {
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
       if (res.links[i].href !== null){
         links.push(res.links[i].href)
       }
     }
-    msg.react('✅')
+    msg.react('✅').catch(logger.error);
     msg.channel.send(`**I found:** ${links.join('\n')}`)
   })
 };
 
 exports.help = {
     category   : 'fun',
-    usage      : '',
+    usage      : false,
     description: 'Search at google',
     detail     : `Look for whatever you want when you want`,
     botPerm    : ['SEND_MESSAGES', "EMBED_LINKS"],
