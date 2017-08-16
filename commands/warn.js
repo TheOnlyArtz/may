1d23ecc5880f94d5fbbbef381d610d6adde17832
TheOnlyArtz <gamesil456@gmail.com>
2017-08-16 19:00:14 +0300

    Adding variables and embeds

--- commands/warn.js
+++ commands/warn.js
@@ -1,5 +1,17 @@
 exports.run = async (client,msg,args) => {
+  const toBanUsr = msg.mentions.users.last() === client.user ? msg.mentions.users.first() : msg.mentions.users.last();
+  let reason;
+  let duration;
+  if (!args[1]){
+      duration = 'permanent';
+      reason = 'None'
+      reason = args.slice(2).join(' ') ? args.slice(2).join(' ') : 'None';
+  } else {
+      reason = args.slice(1).join(' ');
+  }
 
+  const embed = new Discord.RichEmbed()
+  .addField('')
 };
 
 exports.help = {
