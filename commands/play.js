c5fe2d44fb26859fc5b96ba5e2453455bc328f37
TheOnlyArtz <gamesil456@gmail.com>
2017-08-17 12:15:52 +0300

    Moved to new function

--- commands/play.js
+++ commands/play.js
@@ -1,23 +1,15 @@
 /**
 *@function {fetchSongData} fetching data from the video to play it
+*@function {checkGuildVC} Check if the bot connects to a voice channel in the guild
 */
+
 const fetchSongData = require('../functions/music/fetchSongData.js');
+const checkGuildVC = require('../functions/music/checkGuildVC.js');
+
 exports.run = async (client,msg,args) => {
   let searchTerms = args.join(' ');
   let videoId = await fetchSongData(client, msg, searchTerms);
-
-  if (videoId) {
-      if (!this.message.guild.voiceConnection) {
-        this.message.member.voiceChannel.join()
-          .then(async connection => {
-            logger.info(`Started to stream ${chalk.magenta(r.body.items[0].title)} for ${this.message.author.username}`);
-            play(connection, this.message);
-        });
-    }
-
-  } else {
-    return;
-  }
+  await checkGuildVC(client, msg);
 };
 
 exports.help = {
