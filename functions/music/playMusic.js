90085df5efd749f93faf481f3ac1aab20be79e3a
TheOnlyArtz <gamesil456@gmail.com>
2017-08-17 14:22:21 +0300

    queue  cache reloadation

--- functions/music/playMusic.js
+++ functions/music/playMusic.js
@@ -2,29 +2,30 @@ const ytdl = require('ytdl-core');
 const jsonDatabase = require('node-json-db');
 const db = new jsonDatabase('./musicQueue/queue.json', true, true);
 
-db.reload();
 async function playMusic(connection, msg) {
-  let nextSong = db.getData(`guilds/${msg.guild.id}/queue`);
-  dispatcher = connection.playStream(ytdl(nextSong[0], {filter: 'audioonly'}));
+  db.reload();
 
+  let nextSong = db.getData(`/guilds/${msg.guild.id}/queue`);
+  console.log(nextSong);
+  dispatcher = connection.playStream(ytdl(nextSong[0], {filter: 'audioonly'}));
   setTimeout(async function () {
-    await db.delete(`guilds/${msg.guild.id}/queue[0]`);
-  }, 200);
+    await db.delete(`/guilds/${msg.guild.id}/queue[0]`);
+  }, 1000);
 
   dispatcher.on('end', async () => {
-    if (nextSong[0]) {
+    if (nextSong) {
       try {
         await playMusic(connection, msg);
       } catch (e) {
         logger.error(e)
-        db.delete(`guilds/${msg.guild.id}/queue`)
+        db.delete(`/guilds/${msg.guild.id}/queue`)
         connection.disconnect();
       }
 
     } else {
       console.log(nextSong);
       connection.disconnect();
-      db.delete(`guilds/${msg.guild.id}/queue`);
+      db.delete(`/guilds/${msg.guild.id}/queue`);
     }
   });
 }
--- functions/music/pushSongs.js
+++ functions/music/pushSongs.js
@@ -1,15 +1,15 @@
 const jsonDatabase = require('node-json-db')
 const db = new jsonDatabase('./musicQueue/queue.json', true, true);
-db.reload();
   async function pushSongs(msg, songID) {
+    db.reload();
   try {
-    await db.getData(`guilds/${msg.guild.id}/queue`)
+    await db.getData(`/guilds/${msg.guild.id}/queue`)
   } catch (e) {
-    await db.push(`guilds/${msg.guild.id}`, {queue: []}, false);
+    await db.push(`/guilds/${msg.guild.id}`, {queue: []}, false);
   }
 
   try {
-    db.push(`guilds/${msg.guild.id}`, {queue: [songID]}, false); // Push the 1st Item it found
+    db.push(`/guilds/${msg.guild.id}`, {queue: [songID]}, false); // Push the 1st Item it found
   } catch (e) {
     console.error(e);
   }
