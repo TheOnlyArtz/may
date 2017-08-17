d637216ac8793221d76e8625b8c11452a1b2f720
TheOnlyArtz <gamesil456@gmail.com>
2017-08-17 14:58:43 +0300

    Testing messages for videos

--- commands/play.js
+++ commands/play.js
@@ -6,17 +6,24 @@
 const fetchSongData = require('../functions/music/fetchSongData.js');
 const checkGuildVC = require('../functions/music/checkGuildVC.js');
 const pushSongs = require('../functions/music/pushSongs.js');
-
+// const InfoFetcher = require('youtube-info');
+// const moment = require('moment');
+// const Discord = require('discord.js')
 exports.run = async (client,msg,args) => {
   msg.delete();
   let searchTerms = args.join(' ');
   let videoId = await fetchSongData(client, msg, searchTerms);
   if (!msg.member.voiceChannel) {
-    return msg.reply('You must be in a voiceChannel')
+    return msg.reply('You must be in a voiceChannel');
   }
   if (videoId) {
     await pushSongs(msg, videoId);
     await checkGuildVC(client, msg);
+    // let yInfo = await InfoFetcher(videoId);
+    // console.log(yInfo);
+    // const embed = new Discord.RichEmbed()
+    // .setThumbnail(videoId.thumbnailUrl)
+    // .addField('Song Info', `**Uploaded By:** ${videoId.owner}\n**Duration:** ${moment(videoId.duration).format('mm:ss')}`)
   } else {
     return;
   }
