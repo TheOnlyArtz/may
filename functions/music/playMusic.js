df554649aea4f6acaf055be74c62a9ecf6a2c525
TheOnlyArtz <gamesil456@gmail.com>
2017-08-17 13:56:22 +0300

    QUEUE now works!!

--- functions/music/playMusic.js
+++ functions/music/playMusic.js
@@ -1,26 +1,30 @@
 const ytdl = require('ytdl-core');
-const jsonDatabase = require('node-json-db')
+const jsonDatabase = require('node-json-db');
 const db = new jsonDatabase('./musicQueue/queue.json', true, true);
+
 async function playMusic(connection, msg) {
-  let nextSong = db.getData(`parent/${msg.guild.id}/queue`);
+  db.reload();
+  let nextSong = db.getData(`guilds/${msg.guild.id}/queue`);
   dispatcher = connection.playStream(ytdl(nextSong[0], {filter: 'audioonly'}));
 
   setTimeout(async function () {
-    await db.delete(`parent/${msg.guild.id}/queue[0]`);
-  }, 500);
+    await db.delete(`guilds/${msg.guild.id}/queue[0]`);
+  }, 3000);
 
   dispatcher.on('end', async () => {
-    if (nextSong[0] || nextSong.length > 0) {
+    if (nextSong[0]) {
       try {
-        await play(playMusic, msg);
+        await playMusic(connection, msg);
       } catch (e) {
-        db.delete(`/parent/${msg.guild.id}`)
+        logger.error(e)
+        db.delete(`guilds/${msg.guild.id}/queue`)
         connection.disconnect();
       }
 
     } else {
+      console.log(nextSong);
       connection.disconnect();
-      db.delete(`/parent/${msg.guild.id}`);
+      db.delete(`guilds/${msg.guild.id}/queue`);
     }
   });
 }
