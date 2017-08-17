80bdc4d3274df72733a4fa4bfe975599d9c77720
TheOnlyArtz <gamesil456@gmail.com>
2017-08-17 12:52:11 +0300

    play function

--- functions/music/playMusic.js
+++ functions/music/playMusic.js
@@ -1,8 +1,29 @@
 const ytdl = require('ytdl-core');
 const jsonDatabase = require('node-json-db')
-const db = new jsonDatabase('../../musicQueue/queue.json', true, true);
-async function playMusic(msg, client, songID) {
+const db = new jsonDatabase('./musicQueue/queue.json', true, true);
+async function playMusic(connection, msg) {
+  let nextSong = db.getData(`parent/${msg.guild.id}/queue[0]`);
+  dispatcher = connection.playStream(ytdl(nextSong, {filter: 'audioonly'}));
 
+  setTimeout(() =>  {
+    db.delete(`parent/${msg.guild.id}/queue[0]`);
+  }, 1000);
+
+  db.reload();
+  dispatcher.on('end', async () => {
+    if (nextSong) {
+      try {
+        await play(playMusic, msg);
+      } catch (e) {
+        db.delete(`/parent/${message.guild.id}`)
+        connection.disconnect();
+      }
+
+    } else {
+      connection.disconnect();
+      db.delete(`/parent/${message.guild.id}`);
+    }
+  });
 }
 
 module.exports = playMusic
