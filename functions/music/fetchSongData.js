a63f867398d26635e72dbea3006bb0ea1d4ed98f
TheOnlyArtz <gamesil456@gmail.com>
2017-08-17 10:15:58 +0300

    added variables

--- functions/music/fetchSongData.js
+++ functions/music/fetchSongData.js
@@ -1,3 +1,7 @@
+/** @ignore */
+const apiKey = require('.../config/config.json')
+const ytPI         = require('simple-youtube-api');
+const videoFetcher = new ytPI(apiKey)
 function fetchSongData(client, msg, toSearch) {
 
 }
