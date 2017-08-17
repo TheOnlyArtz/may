b1995a6cbeb4036127b840c3f81de4f2fd2e820c
TheOnlyArtz <gamesil456@gmail.com>
2017-08-17 10:59:30 +0300

    catch error

--- functions/music/fetchSongData.js
+++ functions/music/fetchSongData.js
@@ -2,9 +2,10 @@
 const apiKey = require('../../config/config.json').YOUTUBEAPIKEY
 const ytPI         = require('simple-youtube-api');
 const videoFetcher = new ytPI(apiKey)
+const sf = require('snekfetch');
 
 function fetchSongData(client, msg, toSearch) {
-  
+
   /**
   * @param {String} toSearch Getting the right video to play
   * @param {String} apiKey getting access to the data
@@ -13,8 +14,10 @@ function fetchSongData(client, msg, toSearch) {
   */
   sf.get(`https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=` + encodeURIComponent(toSearch) + '&key=' + apiKey)
     .then(async response => {
-      logger.debug(response.body);
-  });
+      logger.log(Object.keys(response.body.items));
+  }).catch(e => {
+    logger.error(e)
+  })
 }
 
 module.exports = fetchSongData;
