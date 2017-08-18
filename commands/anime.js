fd810c0d6e62fc42485080791237206cf70b2026
TheOnlyArtz <gamesil456@gmail.com>
2017-08-18 14:25:39 +0300

    Adding anime

---
+++ commands/anime.js
@@ -0,0 +1,17 @@
+const cheerio = require('cheerio');
+const request = require('request');
+urls = [];
+exports.run = async(client, msg, args) => {
+
+}
+exports.help = {
+    category   : 'fun',
+    usage      : '',
+    description: 'Random anime pic',
+    detail     : 'Displaying a random anime picture',
+    botPerm    : ['SEND_MESSAGES', 'EMBED_LINKS'],
+    authorPerm : [],
+    alias      : [
+        null
+    ]
+};
