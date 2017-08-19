f1bdbc3747ba7e18dd01d892fd2da1ef9fd60f1d
Charlie <charlie.tiehm@gmail.com>
2017-08-19 18:37:19 +0200

    Anime

--- commands/anime.js
+++ commands/anime.js
@@ -1,12 +1,14 @@
-const cheerio = require('cheerio');
-const request = require('request');
-urls = [];
+const sf = require('snekfetch');
 exports.run = async(client, msg, args) => {
-
-}
+    if (cooldown(msg, 'anime', 60, 'This command has a cooldown of **1 Minute**!')) {
+        let res = await sf.get('http://api.cutegirls.moe/json');
+        if (res.body.status !== 200) return msg.channel.send('An error occurred while processing this command.');
+        msg.channel.send({files: [res.body.data.image]});
+    }
+};
 exports.help = {
     category   : 'fun',
-    usage      : '',
+    usage      : false,
     description: 'Random anime pic',
     detail     : 'Displaying a random anime picture',
     botPerm    : ['SEND_MESSAGES', 'EMBED_LINKS'],
