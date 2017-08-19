fad688937855244a0734d1354379cd1dadf57478
Charlie <charlie.tiehm@gmail.com>
2017-08-19 19:07:10 +0200

    I dun want the stream part

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ exports.run = (client) => {
     logger.info('May is ready to use');
     logger.debug(`Logged in as ${client.user.tag}`);
     logger.debug(`Serving ${client.guilds.size} servers with ${client.users.filter(i => !i.bot).size} users`);
-    client.user.setPresence({ game: { name: 'tests', type: 1, url : 'https://twitch.tv/theonlyartz' } }).catch( err => logger.error(err));
+    client.user.setPresence({ game: { name: 'tests', type: 0 } }).catch( err => logger.error(err));
 };
