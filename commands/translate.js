c7b9016b85bc1a07f48f75b512dbc3e7876238f4
Charlie <charlie.tiehm@gmail.com>
2017-08-23 01:16:16 +0200

    Indentation

--- commands/translate.js
+++ commands/translate.js
@@ -3,27 +3,27 @@ const config = require('../config/config.json').PREFIX;
 const Discord = require('discord.js');
 exports.run = async (client,msg,args) => {
 
-  let toTrans = msg.content.split(" ").slice(1);
-  let language;
-  language = toTrans[toTrans.length -2] === 'to' ? toTrans.slice(toTrans.length -2, toTrans.length)[1].trim() : undefined;
-  if (!language) {
-    return msg.reply(`Please supply valid agruments.\n**Example** \`${config}translate May is my favorite bot to english\``)
-  }
-  let finalToTrans = toTrans.slice(toTrans.length - toTrans.length, toTrans.length - 2).join(" ");
-  translate(finalToTrans, {to: language}).then(res => {
-      const embed = new Discord.RichEmbed()
-      .setAuthor('May\'s translator', client.user.displayAvatarURL)
-      .setColor(Math.floor((Math.random() * 1600000) + 6))
-      .addField('Translator:', `**From:** ${res.from.language.iso}\n\`\`\`${finalToTrans}\`\`\`\n**To: **${language}\n\`\`\`${res.text}\`\`\``);
-      msg.channel.send({embed})
-  }).catch(err => {
-    msg.channel.send({
-      embed : {
-        description : "❌ We could not find the supplied language.",
-        color       : 0xe8642b
-      }
-    })
-  });
+    let toTrans = msg.content.split(" ").slice(1);
+    let language;
+    language = toTrans[toTrans.length -2] === 'to' ? toTrans.slice(toTrans.length -2, toTrans.length)[1].trim() : undefined;
+    if (!language) {
+        return msg.reply(`Please supply valid agruments.\n**Example** \`${config}translate May is my favorite bot to english\``)
+    }
+    let finalToTrans = toTrans.slice(toTrans.length - toTrans.length, toTrans.length - 2).join(" ");
+    translate(finalToTrans, {to: language}).then(res => {
+        const embed = new Discord.RichEmbed()
+            .setAuthor('May\'s translator', client.user.displayAvatarURL)
+            .setColor(Math.floor((Math.random() * 1600000) + 6))
+            .addField('Translator:', `**From:** ${res.from.language.iso}\n\`\`\`${finalToTrans}\`\`\`\n**To: **${language}\n\`\`\`${res.text}\`\`\``);
+        msg.channel.send({embed})
+    }).catch(err => {
+        msg.channel.send({
+            embed : {
+                description : "❌ We could not find the supplied language.",
+                color       : 0xe8642b
+            }
+        })
+    });
 };
 
 exports.help = {
