6fa40cae181c3c08a8043dc9eec0966b1d14b325
TheOnlyArtz <gamesil456@gmail.com>
2017-08-22 20:35:10 +0300

    Added instructions for testing

--- functions/permInstructions.js
+++ functions/permInstructions.js
@@ -1,6 +1,11 @@
 function permInstructions(permission) {
 	switch {
-		case ""
+		case "ADMINISTRATOR":
+			return 'https://gyazo.com/fef2ea597650a63a9351c9d763bd1f3e';
+			break;
+		case "CREATE_INSTANT_INVITE":
+			return 'https://gyazo.com/8c6ccec449c29011cf1bfdac3b0b5560'
+			break;
 	}
 }
 
