459046409cf926b4f7655bf1755b7dee623636b6
Charlie <charlie.tiehm@gmail.com>
2017-08-23 01:14:21 +0200

    Indentation

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,7 @@
 /**
 *@function {fetchSongData} fetching data from the video to play it
-*@function {checkGuildVC} Check if the bot connects to a voice channel in the guild
-*/
+ *@function {checkGuildVC} Check if the bot connects to a voice channel in the guild
+ */
 
 const fetchSongData = require('../functions/music/fetchSongData.js');
 const checkGuildVC = require('../functions/music/checkGuildVC.js');
@@ -10,25 +10,25 @@ const InfoFetcher = require('youtube-info');
 const moment = require('moment');
 const Discord = require('discord.js');
 exports.run = async (client,msg,args) => {
-  msg.delete();
-  let searchTerms = args.join(' ');
-  let videoId = await fetchSongData(client, msg, searchTerms);
-  if (!msg.member.voiceChannel) {
-    return msg.reply('You must be in a voiceChannel');
-  }
-  if (videoId) {
-      await pushSongs(msg, videoId);
-      await checkGuildVC(client, msg);
-      let yInfo = await InfoFetcher(videoId);
-      const embed = new Discord.RichEmbed()
-      .setColor(0x00abe0)
-      .setAuthor('Added new song to the queue', client.user.displayAvatarURL)
-      .setThumbnail(yInfo.thumbnailUrl)
-      .addField('Song Info', `**Uploaded By:** ${yInfo.owner}\n**Duration:** ${(yInfo.duration / 60).toFixed(2)} Minutes\n**Views:** ${yInfo.views}`);
-      msg.channel.send({embed})
-  } else {
+    msg.delete();
+    let searchTerms = args.join(' ');
+    let videoId = await fetchSongData(client, msg, searchTerms);
+    if (!msg.member.voiceChannel) {
+        return msg.reply('You must be in a voiceChannel');
+    }
+    if (videoId) {
+        await pushSongs(msg, videoId);
+        await checkGuildVC(client, msg);
+        let yInfo = await InfoFetcher(videoId);
+        const embed = new Discord.RichEmbed()
+            .setColor(0x00abe0)
+            .setAuthor('Added new song to the queue', client.user.displayAvatarURL)
+            .setThumbnail(yInfo.thumbnailUrl)
+            .addField('Song Info', `**Uploaded By:** ${yInfo.owner}\n**Duration:** ${(yInfo.duration / 60).toFixed(2)} Minutes\n**Views:** ${yInfo.views}`);
+        msg.channel.send({embed})
+    } else {
 
-  }
+    }
 };
 
 exports.help = {
