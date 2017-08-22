be8a5757d862b6c1e924cbecb745cbd99aa335a8
Charlie <charlie.tiehm@gmail.com>
2017-08-23 01:15:57 +0200

    Indentation

--- commands/skip.js
+++ commands/skip.js
@@ -5,37 +5,37 @@ let shuffReq = 0;
 exports.run = async (client, msg) => {
 
 
-	queue.reload();
-	if (!shuffler.includes(msg.author.id)) {
-		shuffler.push(msg.author.id);
-		if (!msg.member.voiceChannel) {
-			return msg.reply('You can\' skip since you aren\'t In a voice channel!');
-		}
-    shuffReq++;
-		if (shuffReq >= Math.ceil((msg.member.voiceChannel.members.size - 1) / 2)) {
+    queue.reload();
+    if (!shuffler.includes(msg.author.id)) {
+        shuffler.push(msg.author.id);
+        if (!msg.member.voiceChannel) {
+            return msg.reply('You can\' skip since you aren\'t In a voice channel!');
+        }
+        shuffReq++;
+        if (shuffReq >= Math.ceil((msg.member.voiceChannel.members.size - 1) / 2)) {
 
-      try {
-        let p = queue.getData(`/guilds/${msg.guild.id}/queue[0]`)
-      } catch (e) {
-        return msg.reply('No songs are currently in the play list');
-      }
+            try {
+                let p = queue.getData(`/guilds/${msg.guild.id}/queue[0]`)
+            } catch (e) {
+                return msg.reply('No songs are currently in the play list');
+            }
 
 
-      try {
-        dispatcher.end();
-				shuffReq = 0;
-				shuffler = [];
-				msg.reply('Skipped on the song successfully!');
-				logger.info(`${msg.author.username} Skipped successfully on the song`);
-			} catch (e) {
-        logger.error(e);
-				msg.channel.send('**No songs are currently playing!**');
-			}
-  			} else {
-  				msg.reply(`Hey ${msg.author.username}, Your skip as been added to the list\n\
+            try {
+                dispatcher.end();
+                shuffReq = 0;
+                shuffler = [];
+                msg.reply('Skipped on the song successfully!');
+                logger.info(`${msg.author.username} Skipped successfully on the song`);
+            } catch (e) {
+                logger.error(e);
+                msg.channel.send('**No songs are currently playing!**');
+            }
+        } else {
+            msg.reply(`Hey ${msg.author.username}, Your skip as been added to the list\n\
   you need ` + Math.ceil(((msg.member.voiceChannel.members.size - 1) / 2) - shuffReq) + ' Guy(s) to skip the song');
-  			}
-  	}
+        }
+    }
 };
 
 
