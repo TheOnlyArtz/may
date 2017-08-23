364fa6bb041b10478d71251766c67dca86d16921
TheOnlyArtz <gamesil456@gmail.com>
2017-08-23 11:06:18 +0300

    New messages

--- functions/checkPerms.js
+++ functions/checkPerms.js
@@ -16,15 +16,15 @@ function checkForPermissions(botPermissions, authorPermissions, cmd, msg, client
   let botInstructions = [];
   for (let i = 0; i < cmd.help.botPerm.length; i++) {
     if (!msg.guild.member(client.user).hasPermission(botPermissions[i])) {
-      botInstructions.push(`${instructions(botPermissions[i])}`)
+      botInstructions.push(`${instructions[botPermissions[i]]}`)
       botPermsMissing.push(botPermissions[i]);
     }
   }
   if (botPermsMissing[0]) {
     if(!msg.guild.member(client.user).hasPermission(botPermsMissing)) {
-      msg.channel.send(`= ❌Missing permissions❌ =\nMissing List: ${botPermsMissing.join(', ')}\n= Need Help? = \n${botInstructions.join('\n')}`,{code : "asciidoc"})
+      msg.channel.send(`= ❌Missing permissions❌ =\nMissing List: ${botPermsMissing.join(', ')}\n= Please turn on these permissions for may = \n${botInstructions.join('\n')}`,{code : "asciidoc"})
         .catch(logger.error);
-      // msg.channel.send(`Cannot perform ${cmd.name} missing permissions => (${botPermsMissing.join(' ')})`).catch(logger.error)
+      msg.channel.send('Please read that if you do not have knowledge about roles <https://support.discordapp.com/hc/en-us/articles/214836687-Role-Management-101>')
       return true
   }
 }
