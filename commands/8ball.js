18270a2544f362e3575f8f5eee29bc194e99a1cf
Charlie <charlie.tiehm@gmail.com>
2017-08-24 22:56:39 +0200

    JSDocs

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,18 +1,17 @@
+/**
+ * @member {Object}
+ */
 const snekfetch = require('snekfetch');
 
 exports.run = async (client, msg, args) => {
-    // Handle error and ask for arguments
     if (!args.join(' ')) {
         return msg.channel.send('Please include a question');
     }
-
-    /**
-    * @param {String} url fetch data from
-    * @returns {Promise}
-    * @returns {Error}
-    */
     let r = await snekfetch.get('https://8ball.delegator.com/magic/JSON/0');
     let answerBall = r.body;
+    /**
+     * @param {{magic:object}} The returned Object from API
+     */
     let ball = answerBall.magic.answer;
     msg.channel.send('**[8 Ball]** :crystal_ball: ' + ball);
 };
