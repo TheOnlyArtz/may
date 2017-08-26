112cb067b8d443c08510421dda951cde378c9189
TheOnlyArtz <gamesil456@gmail.com>
2017-08-26 21:02:08 +0300

    added config

---
+++ commands/history.js
@@ -0,0 +1,16 @@
+exports.run = async (client, msg, args) => {
+
+};
+
+exports.help = {
+    category: 'moderation',
+    usage: false,
+    description: 'Shows mentioned user punishment history',
+    detail: 'When using history you can take a look at detailed amount of punishments of any kind the user got',
+    botPerm: ['SEND_MESSAGES'],
+    authorPerm: [],
+    example: '@user#5743',
+    alias: [
+        null
+    ]
+};
