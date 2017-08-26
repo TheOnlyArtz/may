7343324684b4b93cea3bf032b5e0ac6efa200161
TheOnlyArtz <gamesil456@gmail.com>
2017-08-26 19:27:17 +0300

    JSDOCS and variables

--- functions/modhistory.js
+++ functions/modhistory.js
@@ -1,1 +1,14 @@
 // A function for modhistory table
+let table = r.table('ModHistory');
+/**
+* @param {String} column - What column to update
+* @param {String} guild - guild would be the ID of the guild
+* @param {String} user - user would be the ID of the user
+*/
+async function updateModHistory(column, guild, user) {
+  try {
+      let exists = await table.filter({userID : user}).run()[0];
+  } catch (e) {
+
+  }
+}
