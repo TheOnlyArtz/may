0211f1934f89fbab74c14f70ca62c8921f6b798b
TheOnlyArtz <gamesil456@gmail.com>
2017-08-27 12:11:05 +0300

    Made it working but just need a way of adding object ot the array

--- commands/tag.js
+++ commands/tag.js
@@ -1,13 +1,38 @@
 const table = r.table('tags');
 exports.run = async (client, msg, args) => {
+  let exists = await table.filter({guildID : msg.guild.id, userID : msg.author.id}).run();
 
-  const action = args[0];
-  const name   = args[0] ? args[1] : undefined;
-  
-  /**
-  *
-  *
-  */
+  const action  = args[0];
+  const name    = args[0] ? args[1] : undefined;
+  const content = name ? msg.content.split(' ').slice(3).join(' ') : undefined;
+
+   if (action === 'register') {
+      console.log(name, "NAME", content, "CONTENT");
+      if (!exists[0]) {
+        await table.insert({
+          userID  : msg.author.id,
+          guildID : msg.guild.id,
+          tags    : [{
+            name    : name,
+            content : content
+          }]
+        }).run()
+      } else {
+        // TODO: Find a way of adding object to the array and not deleting it
+        await table.update({
+          userID  : msg.author.id,
+          guildID : msg.guild.id,
+          tags : [{
+            name    : name,
+            content : content
+          }]
+        }).run()
+      }
+   } else {
+     let tagName = action
+     let tagValue = exists[0]['tags'].filter(o => o.name === tagName)[0]['content']
+     msg.channel.send(tagValue)
+   }
 
 };
 
