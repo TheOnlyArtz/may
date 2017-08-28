fdd20205aa7c91e4b7cc150984372faa71238eef
TheOnlyArtz <gamesil456@gmail.com>
2017-08-28 17:31:19 +0300

    Variables  / JSdocs / start

--- functions/rethinkTimers.js
+++ functions/rethinkTimers.js
@@ -1,4 +1,16 @@
-function timer(table, doc, time, howmany) {
+const ms   = require('ms');
+const time = require('moment');
+
+/**
+* @param {String} time - the unformatted time of the punishment
+* @param {String} table - what table to look into
+* @param {String} user - the selected user ID
+*/
+async function timer(time, table, user) {
+  /*
+  * Get the inserted time of the user from the database.
+  */
+  let userObj = await r.table(table).filter({userID : user}).run()[0];
 
 }
 
