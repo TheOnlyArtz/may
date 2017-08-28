72d0cec8466e24215744b29fcc5d319152f003f3
TheOnlyArtz <gamesil456@gmail.com>
2017-08-28 18:26:19 +0300

    Wrote the whole function

--- functions/rethinkTimers.js
+++ functions/rethinkTimers.js
@@ -5,13 +5,44 @@ const time = require('moment');
 * @param {String} time - the unformatted time of the punishment
 * @param {String} table - what table to look into
 * @param {String} user - the selected user ID
+* @param {String} guild - insert the guildID
+* @param {String} punishment - the punishment for the user
 */
-async function timer(time, table, user) {
-  /*
-  * Get the inserted time of the user from the database.
-  */
-  let userObj = await r.table(table).filter({userID : user}).run()[0];
+async function timer(time, table, user, punishment, guild) {
 
+  /**
+  * @param {String} table - what table to look into
+  * @param {String} user - filter with the userID
+  * @param {String} guild - filter with the guildID
+  */
+  let exists = await r.table(table).filter({
+    guildID : guild, userID : user
+  }).run()[0];
+
+  if (!exists) {
+    /**
+    * @param {String} table - what table to look into
+    * @param {String} user - insert the userID
+    * @param {String} guild - insert the guildID
+    */
+    await r.table(table).insert({
+      guildID : guild,
+      userID : user
+    }).run();
+  }
+
+
+  /*
+  * Get the data when the punish should end
+  */
+  let unformattedUnix = moment().add(ms(time), 'ms').format('DD/MM/YYYY HH:mm:ss');
+
+  /*
+  * Insert the time of the punishment to the database.
+  */
+  await r.table(table).filter({userID : user}).insert({
+    [punishment] : (new Date(unformattedUnix)).getTime(); //Inserts UNIX formatted timestamp
+  }).run();
 }
 
 module.exports = timer;
