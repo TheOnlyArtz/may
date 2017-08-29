8003453553a11bbb0110568fe960edcec27a3e1f
TheOnlyArtz <gamesil456@gmail.com>
2017-08-29 10:01:09 +0300

    Semicolons, protect bugs when deleting document and another punish is in

--- functions/punishmentInterval.js
+++ functions/punishmentInterval.js
@@ -4,21 +4,27 @@ const moment = require('moment');
     setInterval(async () => {
       let arr = await r.table('timers').filter({userID : "NONE"}).run();
       if (arr[0].inPunishQueue[0]) {
+
           for (var i = 0; i < arr[0].inPunishQueue.length; i++) {
+
             let guildID = arr[0].inPunishQueue[i]['guildID'];
             let userID = arr[0].inPunishQueue[i]['userID'];
             let usersUnix = await r.table('timers').filter({
               guildID : guildID,
               userID  : userID
             }).run()
+
             if (usersUnix[0].mute &&(usersUnix[0].mute < Date.now())) {
-              logger.info('Removed new muted role [auto]')
+
+              logger.info('Removed new muted role [auto]');
               let muteRole = client.guilds.get(guildID).roles.find('name', 'may-muted');
               client.guilds.get(guildID).members.get(userID).removeRole(muteRole);
-              let p = arr[0].inPunishQueue
+              let p = arr[0].inPunishQueue;
+
               function findInd(element) {
-                return element.userID === userID && element.guildID === guildID
+                return element.userID === userID && element.guildID === guildID;
               }
+
               p.findIndex(findInd)
 
               let appendToArray = (table, uArray) => r.table(table)
@@ -27,10 +33,20 @@ const moment = require('moment');
              .default([]).deleteAt(p.findIndex(findInd)) }))
              .run();
 
+             if (usersUnix[0].ban) {
+               await r.table("timers")
+               .filter({guildID : msg.guild.id, userID : msg.author.id})
+               .update({mute : null}).run();
+             } else {
 
-            await r.table("timers")
-            .filter({guildID : msg.guild.id, userID : msg.author.id})
-            .update({mute : null}).run();
+               //Else, delete the document
+               appendToArray('timers', 'inPunishQueue');
+               await r.table('timers').filter({
+                 guildID : guildID,
+                 userID  : userID
+               }).delete().run();
+
+              }
             }
             if (usersUnix[0].ban && (usersUnix[0].ban < Date.now())) {
               logger.info('Unbanned new user [auto]');
