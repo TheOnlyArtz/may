720086e9d9a37eb7f7a1c100817325bcb1aa6a7f
TheOnlyArtz <gamesil456@gmail.com>
2017-08-30 23:08:50 +0300

    Preapering for a function rewrite

--- functions/twitch.js
+++ functions/twitch.js
@@ -3,49 +3,10 @@ const twitchClass = require('../classes/twitch.js');
 const twitch = new twitchClass(config.CLIENTID);
 
 let check = async (bot) => {
-
-    let rows = await r.table('livestreams').run();
-    /*
-    rows returns an array
-    */
-    for (let row of rows) {
-        /*
-        get one server
-         */
-        if (row.streams) {
-            for (let a of row.livestreams) {
-                /*
-                get every stream from one server
-                 */
-                twitch.check(a.name).then(data => {
-                    console.log(data);
-                    let p =  row.livestreams;
-                    function findInd(element) {
-                        return element.name === data.name;
-                    }
-                    let toInsert = data.online ? {
-                        game: data.game,
-                        views: data.views,
-                        image: data.image,
-                        mature: data.mature,
-                        lang: data.lang,
-                        name: data.name,
-                        url: data.url,
-                        msgID: null,
-                        online: true
-                    } : {online: false, name: data.name};
-
-                    let appendToArray = (table, uArray, doc) => r.table(table)
-                        .filter({guildID : msg.guild.id})
-                        .update(object => ({ [uArray]: object(uArray)
-                            .default([]).changeAt(p.findIndex(findInd), toInsert) }))
-                        .run();
-                    appendToArray('livestreams', 'livestreams')
-                })
-            }
-        }
-    }
-
+    setInterval(() => {
+        let queue = await r.table('livestreams').getAll('NONE', {index : 'guildID'});
+        
+    }, 7 * 60000);
 };
 
 module.exports = check;
