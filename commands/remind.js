222bcea514d399355f833e3119ce19a51b0a055d
TheOnlyArtz <gamesil456@gmail.com>
2017-08-30 11:09:15 +0300

    Wrote the database integration

--- commands/remind.js
+++ commands/remind.js
@@ -1,15 +1,34 @@
-const table = r.table("reminders")
+const table = r.table("reminders");
+const ms = require('ms');
+const moment = require('moment');
 exports.run = async (client, msg, args) => {
     const time    = args[0];
     const content = msg.content.split(' ').slice(2).join(' ');
     if (!time) {
-        return msg.reply('Please include time as first argument')
+        return msg.reply('Please include time as first argument.');
     } else if (!time.match(/\d{1,2}(hour|h|hours|second|sec|s|seconds|d|days|day|m|minutes)\b/)) {
         return msg.reply('Please include **valid** time, look at the example `-help remind`');
     }
 
-    const exists = await table.orderBy({index: msg.guild.id}).run()[0]
-    console.log(exists);
+    if (!content) {
+      return msg.reply('Please supply the content of the reminder too.');
+    }
+    const exists = await table.getAll(msg.guild.id, msg.author.id, {index: "guildID", index: "userID"}).run();
+    if (!exists[0]) {
+      try {
+        let unformattedUnix = moment().add(ms(ms(time)), 'ms');
+        await table.insert({
+          userID : msg.author.id,
+          guildID : msg.guild.id,
+          content : content,
+          time    : new Date(unformattedUnix).getTime(),
+        }).run()
+      } catch (e) {
+        logger.error('Failed to insert reminder into the database', e);
+      }
+    } else {
+        msg.reply('You already have 1 reminder in queue!');
+    }
 };
 
 exports.help = {
