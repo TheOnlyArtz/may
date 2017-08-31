5e3d52cc9701052e21583da4e3337c7c1e489eb5
Charlie <charlie.tiehm@gmail.com>
2017-08-31 14:56:37 +0200

    Cleanup

--- functions/reminderInteral.js
+++ functions/reminderInteral.js
@@ -1,48 +1,48 @@
 async function reminder(client) {
     setInterval(async () => {
-      /**
-      * @param {String} reminders - the table
-      * @param {String} NONE - the Index value
-      * @param {String} guildID - Table's index
-      */
-      let arr = await r.table('reminders').getAll("NONE", {index : "guildID"}).run();
+        /**
+         * @param {String} reminders - the table
+         * @param {String} NONE - the Index value
+         * @param {String} guildID - Table's index
+         */
+        let arr = await r.table('reminders').getAll("NONE", {index : "guildID"}).run();
 
-      //Check if the array is empty
-      if (arr[0].inQueue[0]) {
-          for (let i = 0; i < arr[0].inQueue.length; i++) {
-            /**
-            * @var {Array|Object|String} - Get the guildID from the object
-            * @var {Array|Object|String} - Get the userID from the object
-            */
-            const guildID = arr[0].inQueue[i]['guildID'];
-            const userID = arr[0].inQueue[i]['userID'];
-            const usersUnix = await r.table('reminders').getAll(guildID, userID, {index: "guildID", index: "userID"}).run();
+        //Check if the array is empty
+        if (arr[0].inQueue[0]) {
+            for (let i = 0; i < arr[0].inQueue.length; i++) {
+                /**
+                 * @var {Array|Object|String} - Get the guildID from the object
+                 * @var {Array|Object|String} - Get the userID from the object
+                 */
+                const guildID = arr[0].inQueue[i]['guildID'];
+                const userID = arr[0].inQueue[i]['userID'];
+                const usersUnix = await r.table('reminders').getAll(guildID, userID, {index: "guildID", index: "userID"}).run();
 
-            // Check  for the time and where to trigger the reminder
-            if (usersUnix[0].time &&(usersUnix[0].time < Date.now())) {
-              try {
+                // Check  for the time and where to trigger the reminder
+                if (usersUnix[0].time &&(usersUnix[0].time < Date.now())) {
+                    try {
 
-                  let guild = client.guilds.get(guildID);
-                  let member = await guild.fetchMember(userID);
-                  client.channels.get(usersUnix[0].channelID).send('**Your reminder:** ' + member + ' ' + usersUnix[0].content);
+                        let guild = client.guilds.get(guildID);
+                        let member = await guild.fetchMember(userID);
+                        client.channels.get(usersUnix[0].channelID).send('**Your reminder:** ' + member + ' ' + usersUnix[0].content);
 
-              } catch (e) {
-                  logger.error(e)
-              }
-              await r.table('reminders').getAll(guildID, userID, {index: "guildID", index: "userID"})
-              .delete()
-              .run();
-              let appendToArray = (table, uArray) => r.table(table)
-             .filter({guildID : "NONE"})
-             .update(object => ({ [uArray]: object(uArray)
-             .default([]).deleteAt(i) }))
-             .run();
-             appendToArray('reminders', 'inQueue')
-          }
-      }
+                    } catch (e) {
+                        logger.error(e)
+                    }
+                    await r.table('reminders').getAll(guildID, userID, {index: "guildID", index: "userID"})
+                        .delete()
+                        .run();
+                    let appendToArray = (table, uArray) => r.table(table)
+                        .filter({guildID : "NONE"})
+                        .update(object => ({ [uArray]: object(uArray)
+                            .default([]).deleteAt(i) }))
+                        .run();
+                    appendToArray('reminders', 'inQueue')
+                }
+            }
 
-    }
-  }, 5 * 1000);
+        }
+    }, 5 * 1000);
 }
 
 module.exports = reminder;
