ae2c1a60e1fca27ccf86c549ef5f8435be992944
TheOnlyArtz <gamesil456@gmail.com>
2017-09-05 19:40:22 +0300

    secure

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,5 +1,19 @@
+const table = r.table('guilds');
 exports.run = async (client, msg, args) => {
+  const action = args[0];
 
+  let enabled = await table.getAll(msg.guild.id , {index : 'guildID'}).run();
+
+
+  if (!enabled[0]) {
+
+    await r.table('guilds').insert({
+      guildID : guild.id,
+      ownerID : guild.owner.user.id,
+    })
+
+    return msg.reply('I got you into the database, Please do the command again')
+  }
   if (args[0] === 'enable') {
     if (enabled[0].nsfw === true) {
       return msg.reply('NSFW is already enabled off for ' + msg.guild.name);
@@ -34,5 +48,7 @@ exports.help = {
     botPerm: ['SEND_MESSAGES'],
     authorPerm: ["ADMINISTRATOR"],
     example: 'Blond anime',
-    alias: []
+    alias: [
+        'r34'
+    ]
 }
